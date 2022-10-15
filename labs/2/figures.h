88b0e29882dfaafba9bb2bef7d013a694950ebb0
Krai53n <greasha46@gmail.com>
2022-10-15 22:03:17 +0300

    replace tabs to spaces

--- labs/2/figures.h
+++ labs/2/figures.h
@@ -3,71 +3,71 @@
 class Figure
 {
 protected:
-	double x, y;
-	double size;
-	int color;
+    double x, y;
+    double size;
+    int color;
 
 public:
-	Figure(double x = 0, double y = 0, int color = 0x000000, double size = 1);
+    Figure(double x = 0, double y = 0, int color = 0x000000, double size = 1);
 
-	double get_x();
-	double get_y();
-	double get_size();
-	int get_color();
+    double get_x();
+    double get_y();
+    double get_size();
+    int get_color();
 
-	void move_to(double x, double y);
-	void move_by(double x, double y);
-	void set_x(double x);
-	void set_y(double y);
-	void set_color(int color);
-	void set_size(double size);
+    void move_to(double x, double y);
+    void move_by(double x, double y);
+    void set_x(double x);
+    void set_y(double y);
+    void set_color(int color);
+    void set_size(double size);
 
-	virtual void draw() = 0;
+    virtual void draw() = 0;
 };
 
 class Circ : public Figure
 {
 public:
-	using Figure::Figure;
-	void draw();
+    using Figure::Figure;
+    void draw();
 };
 
 class Rect : public Figure
 {
 protected:
-	double w, h;
+    double w, h;
 
 public:
-	Rect(double x, double y, double w, double h, int color);
+    Rect(double x, double y, double w, double h, int color);
 
-	double get_w();
-	double get_h();
+    double get_w();
+    double get_h();
 
-	void set_sides(double w, double h);
-	void set_w(double w);
-	void set_h(double h);
+    void set_sides(double w, double h);
+    void set_w(double w);
+    void set_h(double h);
 
-	void draw();
+    void draw();
 };
 
 class Triangle : public Figure
 {
-	double x1, y1, x2, y2, x3, y3;
+    double x1, y1, x2, y2, x3, y3;
 
 public:
-	Triangle(double x1, double y1, double x2, double y2,
-		double x3, double y3, int color);
+    Triangle(double x1, double y1, double x2, double y2,
+        double x3, double y3, int color);
 
-	void move_to(double x1, double y1, double x2, double y2, double x3, double y3);
-	void move_by(double x1, double y1, double x2, double y2, double x3, double y3);
+    void move_to(double x1, double y1, double x2, double y2, double x3, double y3);
+    void move_by(double x1, double y1, double x2, double y2, double x3, double y3);
 
-	void draw();
+    void draw();
 };
 
 class Rhomb : public Rect
 {
 public:
-	using Rect::Rect;
+    using Rect::Rect;
 
-	void draw();
+    void draw();
 };
--- labs/2/main.cpp
+++ labs/2/main.cpp
@@ -5,20 +5,20 @@
 #include "figures.h"
 
 int main() {
-	initwindow(500, 500, "2nd lab", 250, 250);
+    initwindow(500, 500, "2nd lab", 250, 250);
 
-	const int size = 4;
-	Figure* figures[size] = {
-		new Circ(250, 250, 0x0000ff, 100),
-		new Rect(100, 100, 100, 200, 0x00ff00),
-		new Triangle(20, 20, 400, 400, 0, 450, 0xff0000),
-		new Rhomb(100, 100, 200, 200, 0x00ff00),
-	};
+    const int size = 4;
+    Figure* figures[size] = {
+        new Circ(250, 250, 0x0000ff, 100),
+        new Rect(100, 100, 100, 200, 0x00ff00),
+        new Triangle(20, 20, 400, 400, 0, 450, 0xff0000),
+        new Rhomb(100, 100, 200, 200, 0x00ff00),
+    };
 
-	while (true)
-	{
-		setcolor(0x000000);
-		for (auto figure : figures)
-			figure->draw();
-	}
+    while (true)
+    {
+        setcolor(0x000000);
+        for (auto figure : figures)
+            figure->draw();
+    }
 }
